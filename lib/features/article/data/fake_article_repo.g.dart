d7640d8d66affdbba1b94d8c6917e5d17329b49e
obidano <omotetedan2@gmail.com>
2023-12-05 09:34:49 +0100

    add generated files

---
+++ lib/features/article/data/fake_article_repo.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fake_article_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$articleRepoHash() => r'21ffdad2479f7a58cb2bd250a35850272d352656';
+
+/// See also [articleRepo].
+@ProviderFor(articleRepo)
+final articleRepoProvider = AutoDisposeProvider<FakeArticleRepo>.internal(
+  articleRepo,
+  name: r'articleRepoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$articleRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ArticleRepoRef = AutoDisposeProviderRef<FakeArticleRepo>;
+String _$futureArticlesListHash() =>
+    r'0ff3dc3a953e7efeab85fbbb95b5b159415ea1bd';
+
+/// See also [futureArticlesList].
+@ProviderFor(futureArticlesList)
+final futureArticlesListProvider =
+    AutoDisposeFutureProvider<List<Map<String, dynamic>>>.internal(
+  futureArticlesList,
+  name: r'futureArticlesListProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$futureArticlesListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FutureArticlesListRef
+    = AutoDisposeFutureProviderRef<List<Map<String, dynamic>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
